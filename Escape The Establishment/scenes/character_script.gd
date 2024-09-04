116e87152522a735f50978438bbd337cee65f16e
CrispyMD <mdondush@gmail.com>
2024-09-04 19:44:49 +0300

    Changed to third person camera and movement

--- Escape The Establishment/scenes/character_script.gd
+++ Escape The Establishment/scenes/character_script.gd
@@ -3,9 +3,13 @@ extends CharacterBody3D
 const gravity = 9.8
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.003
+const SENSITIVITY = 0.1
 @onready var head = $Head
-@onready var camera = $Head/Camera3D
+#FIRST PERSON
+#@onready var camera = $Head/Camera3D
+@onready var spring = $SpringArm3D
+@onready var camera = $SpringArm3D/Camera3D
+
 
 
 func _ready():
@@ -18,9 +22,14 @@ func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	elif event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
+		self.rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
+		spring.rotate_x(deg_to_rad(-event.relative.y * SENSITIVITY))
+		spring.rotation.x = clamp(spring.rotation.x, deg_to_rad(-90), deg_to_rad(30))
+	#FIRST PERSON
+	#elif event is InputEventMouseMotion:
+		#head.rotate_y(-event.relative.x * SENSITIVITY)
+		#camera.rotate_x(-event.relative.y * SENSITIVITY)
+		#camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
 
 
 
@@ -31,14 +40,13 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.x = clamp(velocity.x, -SPEED, SPEED)
 		velocity.z = direction.z * SPEED
-		velocity.z = clamp(velocity.z, -SPEED, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, 1)
-		velocity.z = move_toward(velocity.z, 0, 1)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
