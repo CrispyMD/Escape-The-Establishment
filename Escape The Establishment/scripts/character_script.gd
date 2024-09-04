18fc506f0b697d6d2ba9637e51ecdb79b32694ee
Bloo <yoav.tovel@gmail.com>
2024-09-04 21:30:35 +0300

    Added more keys to input map, created ability timer

--- Escape The Establishment/scripts/character_script.gd
+++ Escape The Establishment/scripts/character_script.gd
@@ -36,7 +36,7 @@ func _unhandled_input(event):
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
@@ -49,4 +49,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	if Input.is_action_just_pressed("ability"):
+		pass
 	move_and_slide()
